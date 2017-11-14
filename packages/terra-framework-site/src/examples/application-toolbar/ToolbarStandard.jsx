cc611de5bdf31e6214fa11635dbdf6f03fb9162d
Kasper,Dave <Dave.Kasper@Cerner.com>
2017-11-14 14:10:04 -0600

    Add sizing select.

--- packages/terra-framework-site/src/examples/application-toolbar/ToolbarStandard.jsx
+++ packages/terra-framework-site/src/examples/application-toolbar/ToolbarStandard.jsx
@@ -4,41 +4,72 @@ import IconPrinter from 'terra-icon/lib/icon/IconPrinter';
 import IconPill from 'terra-icon/lib/icon/IconPill';
 import NavTabs from 'terra-application-toolbar/lib/NavTabs';
 
-const items = [
-  <Toolbar.Utility.Item
-    text="Utility Menu Item 1"
-    key="Toggle1"
-  />,
-  <Toolbar.Utility.Item
-    text="Utility Menu Item 2"
-    key="Toggle2"
-  />,
-];
+class ToolbarStandard extends React.Component {
+  constructor(props) {
+    super(props);
 
-const toolbar = () => (
-  <Toolbar
-    content={<NavTabs links={[{ path: '123', text: 'Tab 1' }, { path: '234', text: 'Tab 2' }]} />}
-    layoutConfig={{
-      size: 'small',
-      toggleMenu: () => {},
-    }}
-    logo={(
-      <Toolbar.Logo
-        accessory={<IconPill />}
-        title="Title"
-        subtitle="subtitle"
-      />
-    )}
-    utility={(
-      <Toolbar.Utility
-        accessory={<IconPrinter />}
-        contentHeight="80"
-        contentWidth="240"
-        menuItems={items}
-        title="Utility Title"
-      />
-    )}
-  />
-);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.getId = this.getId.bind(this);
+    this.state = { toolbarSize: 'tiny' };
+  }
 
-export default toolbar;
+  getId(name) {
+    return name + this.state.id;
+  }
+
+  handleSelectChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor={this.getId('toolbarSize')}>Toolbar Size</label>
+          <select id={this.getId('toolbarSize')} name="toolbarSize" value={this.state.toolbarSize} onChange={this.handleSelectChange}>
+            <option value={'tiny'}>Tiny</option>
+            <option value={'small'}>Small</option>
+            <option value={'medium'}>Medium</option>
+            <option value={'large'}>Large</option>
+            <option value={'huge'}>Huge</option>
+          </select>
+        </form>
+        <br />
+        <Toolbar
+          content={<NavTabs links={[{ path: '123', text: 'Tab 1' }, { path: '234', text: 'Tab 2' }]} />}
+          layoutConfig={{
+            size: this.state.toolbarSize,
+            toggleMenu: () => {},
+          }}
+          logo={(
+            <Toolbar.Logo
+              accessory={<IconPill />}
+              title="Title"
+              subtitle="subtitle"
+            />
+          )}
+          utility={(
+            <Toolbar.Utility
+              accessory={<IconPrinter />}
+              contentHeight="80"
+              contentWidth="240"
+              menuItems={[
+                <Toolbar.Utility.Item
+                  text="Utility Menu Item 1"
+                  key="Toggle1"
+                />,
+                <Toolbar.Utility.Item
+                  text="Utility Menu Item 2"
+                  key="Toggle2"
+                />,
+              ]}
+              title="Utility Title"
+            />
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+export default ToolbarStandard;
