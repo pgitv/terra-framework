bed38b63de69410432360c2bf6e9683ce01e4582
Kasper,Dave <Dave.Kasper@Cerner.com>
2017-12-01 17:02:29 -0600

    Update to use common placeholder.

--- packages/terra-framework-site/src/examples/application-header-layout/HeaderWireframe.jsx
+++ packages/terra-framework-site/src/examples/application-header-layout/HeaderWireframe.jsx
@@ -1,51 +1,19 @@
 import React from 'react';
 import ApplicationHeaderLayout from 'terra-application-header-layout';
-import PlaceHolder from '../common/PlaceHolder';
+import PlaceHolder from '../../app/common/Placeholder';
 
-class ToolbarStandard extends React.Component {
-  constructor(props) {
-    super(props);
+const HeaderWireframe = () => (
+  <div style={{ height: '60px', position: 'relative', width: '100%' }}>
+    <ApplicationHeaderLayout
+      layoutConfig={{ size: 'large', toggleMenu: () => {} }}
+      logo={<PlaceHolder text="Logo" width="150px" />}
+      utilities={<PlaceHolder text="Utilities" width="150px" />}
+      extensions={<PlaceHolder text="Extensions" width="150px" />}
+      navigation={<PlaceHolder text="Navigation" />}
+      toggle={<PlaceHolder text="Toggle" width="150px" />}
+    />
+  </div>
+);
 
-    this.handleSelectChange = this.handleSelectChange.bind(this);
-    this.getId = this.getId.bind(this);
-    this.state = { headerSize: 'tiny' };
-  }
 
-  getId(name) {
-    return name + this.state.id;
-  }
-
-  handleSelectChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  render() {
-    return (
-      <div>
-        <form>
-          <label htmlFor={this.getId('headerSize')}>Header Size</label>
-          <select id={this.getId('headerSize')} name="headerSize" value={this.state.headerSize} onChange={this.handleSelectChange}>
-            <option value={'tiny'}>Tiny</option>
-            <option value={'small'}>Small</option>
-            <option value={'medium'}>Medium</option>
-            <option value={'large'}>Large</option>
-            <option value={'huge'}>Huge</option>
-          </select>
-        </form>
-        <br />
-        <div style={{ border: '1px solid black', height: '60px', position: 'relative', width: '100%' }}>
-          <ApplicationHeaderLayout
-            layoutConfig={{ size: this.state.headerSize, toggleMenu: () => {} }}
-            logo={<PlaceHolder text="Logo" width="150px" />}
-            utilities={<PlaceHolder text="Utilities" width="150px" />}
-            extensions={<PlaceHolder text="Extensions" width="150px" />}
-            navigation={<PlaceHolder text="Navigation" />}
-            toggle={<PlaceHolder text="Toggle" width="150px" />}
-          />
-        </div>
-      </div>
-    );
-  }
-}
-
-export default ToolbarStandard;
+export default HeaderWireframe;
--- packages/terra-framework-site/src/examples/application-menu-layout/MenuWireframe.jsx
+++ packages/terra-framework-site/src/examples/application-menu-layout/MenuWireframe.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ApplicationMenuLayout from 'terra-application-menu-layout';
-import PlaceHolder from '../common/PlaceHolder';
+import PlaceHolder from '../../app/common/Placeholder';
 
 const Menu = () => (
-  <div style={{ border: '1px solid black', height: '450px', width: '300px' }}>
+  <div style={{ height: '450px', width: '300px' }}>
     <ApplicationMenuLayout
       layoutConfig={{ size: 'tiny', toggleMenu: () => {} }}
       header={<PlaceHolder text="Header" height="50px" />}
