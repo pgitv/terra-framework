43ab6c9eb66990e706cec2b9cd22d562636cb4c1
Kasper,Dave <Dave.Kasper@Cerner.com>
2017-12-04 10:22:32 -0600

    Implement menu layout within site.

--- packages/terra-framework-site/src/app/common/menu/MenuList.jsx
+++ packages/terra-framework-site/src/app/common/menu/MenuList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-import ContentContainer from 'terra-content-container';
+import ApplicationMenuLayout from 'terra-application-menu-layout';
 import RoutingStackDelegate from 'terra-navigation-layout/lib/routing/RoutingStackDelegate';
 import MenuToolbar from './MenuToolbar';
 
@@ -18,35 +18,26 @@ const propTypes = {
 };
 
 const MenuList = ({ routingStackDelegate, links, headerText }) => (
-  <div style={{ height: '100%', width: '100%', position: 'absolute' }}>
-    <ContentContainer
-      header={<MenuToolbar text={headerText} routingStackDelegate={routingStackDelegate} />}
-      fill
-    >
-      <div className="cm-container">
-        <div className="cm-backRegion">
-          {}
-        </div>
-        <div className="cm-contentRegion">
-          <div>
-            {links.map(link => (
-              <NavLink
-                key={link.id}
-                className="cm-link"
-                location={routingStackDelegate.location}
-                to={link.path}
-                activeStyle={{
-                  fontWeight: 'bold',
-                }}
-              >
-                {link.text}
-              </NavLink>
-            ))}
-          </div>
-        </div>
+  <ApplicationMenuLayout
+    header={<MenuToolbar text={headerText} routingStackDelegate={routingStackDelegate} />}
+    content={
+      <div>
+        {links.map(link => (
+          <NavLink
+            key={link.id}
+            className="cm-link"
+            location={routingStackDelegate.location}
+            to={link.path}
+            activeStyle={{
+              fontWeight: 'bold',
+            }}
+          >
+            {link.text}
+          </NavLink>
+        ))}
       </div>
-    </ContentContainer>
-  </div>
+    }
+  />
 );
 
 MenuList.propTypes = propTypes;
