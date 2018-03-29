51c6f3eb472397df8047377713d3a279dfc7a930
tbiethman <tbiethman@gmail.com>
2018-03-29 12:59:11 -0500

    derp.

--- packages/terra-data-grid/src/DataGrid.jsx
+++ packages/terra-data-grid/src/DataGrid.jsx
@@ -10,36 +10,63 @@ const propTypes = {
   test: PropTypes.string,
 };
 
+const Column = ({ name, cellCount }) => (
+  <div className={cx('column')}>
+    {(new Array(cellCount)).fill().map((derp, index) => index === 0 ? (
+      <div className={cx('header-cell')}>{name}</div>
+    ) : (
+      <div className={cx('cell')}>Cell {index}</div>
+    ))}
+  </div>
+);
+
+const columnSet = (num) => {
+  const columns = [];
+  for (let i = 0; i < num; i += 1) {
+    columns.push((
+      <Column name={`Column ${i}`} cellCount={1000} />
+    ));
+  }
+  return columns;
+};
+
 class DataGrid extends React.Component {
   constructor(props) {
     super(props);
+
+    this.updateRefs = this.updateRefs.bind(this);
 
     this.state = {
 
     };
   }
 
+  componentDidMount() {
+    this.updateRefs();
+  }
+
+  updateRefs() {
+    if (this.fixedRef) {
+      this.fixedRef.style.height = `${this.fixedRef.scrollHeight}px`;
+
+      if (this.filledRef) {
+        this.filledRef.style.height = `${this.fixedRef.scrollHeight}px`;
+      }
+    }
+  }
+
+  componentDidUpdate() {
+    this.updateRefs();
+  }
+
   render() {
     return (
-      <div className={cx('container')}>
-        <div className={cx('fixed')}>
-          <div className={cx('column')}>
-            Column 1
-          </div>
-          <div className={cx('column')}>
-            Column 2
-          </div>
+      <div className={cx(['container', 'container-size-1'])}>
+        <div ref={(fixedRef) => { this.fixedRef = fixedRef; }} className={cx('fixed')}>
+          {columnSet(3)}
         </div>
-        <div className={cx('fill')}>
-          <div className={cx('column')}>
-            Column 3
-          </div>
-          <div className={cx('column')}>
-            Column 4
-          </div>
-          <div className={cx('column')}>
-            Column 5
-          </div>
+        <div ref={(filledRef) => { this.filledRef = filledRef; }} className={cx('fill')}>
+          {columnSet(30)}
         </div>
       </div>
     );
