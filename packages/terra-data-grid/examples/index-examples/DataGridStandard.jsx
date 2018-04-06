c5c4e446b3efc496c6150c5759f896a6541d184f
tbiethman <tbiethman@gmail.com>
2018-04-06 16:41:53 -0500

    Adding MPageFusion example and Terra prototype.

--- packages/terra-data-grid/examples/index-examples/DataGridStandard.jsx
+++ packages/terra-data-grid/examples/index-examples/DataGridStandard.jsx
@@ -1,10 +1,144 @@
 import React from 'react';
+
+import MainFile from 'data-grid/dev/MainFile';
+
 import DataGrid from '../../src/DataGrid';
 
-const DataGridDefault = () => (
-  <div style={{ height: '500px', width: '100%' }}>
-    <DataGrid />
-  </div>
-);
+import 'data-grid/node_modules/MPageFusion/dist/css/mpage-fusion.css';
+import 'data-grid/dist/css/DataGrid.css';
 
-export default DataGridDefault;
+class DataGridStandard extends React.Component {
+  componentDidMount() {
+    const mainContainer = new MainFile();
+    mainContainer.mount('data-grid-test').update();
+  }
+
+  render() {
+    const columns = {
+      column1: {
+        startWidth: 200,
+        sortable: true,
+        sortDirection: 'ascending', // 'descending'
+        sortWeight: 'primary', // 'secondary', 'tertiary'
+        resize: true,
+        title: 'Column 1',
+        component: <div>Column 1</div>,
+      },
+      column2: {
+        startWidth: 100,
+        sortable: true,
+        sortDirection: 'ascending', // 'descending'
+        sortWeight: 'primary', // 'secondary', 'tertiary'
+        resize: true,
+        title: 'Column 2',
+        component: <div>Column 2</div>,
+      },
+      column3: {
+        startWidth: 300,
+        sortable: true,
+        sortDirection: 'ascending', // 'descending'
+        sortWeight: 'primary', // 'secondary', 'tertiary'
+        resize: true,
+        title: 'Column 3',
+        component: <div>Column 3</div>,
+      },
+      column4: {
+        startWidth: 400,
+        sortable: true,
+        sortDirection: 'ascending', // 'descending'
+        sortWeight: 'primary', // 'secondary', 'tertiary'
+        resize: true,
+        title: 'Column 4',
+        component: <div>Column 4</div>,
+      },
+      column5: {
+        startWidth: 400,
+        sortable: true,
+        sortDirection: 'ascending', // 'descending'
+        sortWeight: 'primary', // 'secondary', 'tertiary'
+        resize: true,
+        title: 'Column 5',
+        component: <div>Column 5</div>,
+      },
+      column6: {
+        startWidth: 400,
+        sortable: true,
+        sortDirection: 'ascending', // 'descending'
+        sortWeight: 'primary', // 'secondary', 'tertiary'
+        resize: true,
+        title: 'Column 6',
+        component: <div>Column 6</div>,
+      },
+      column7: {
+        startWidth: 400,
+        sortable: true,
+        sortDirection: 'ascending', // 'descending'
+        sortWeight: 'primary', // 'secondary', 'tertiary'
+        resize: true,
+        title: 'Column 7',
+        component: <div>Column 7</div>,
+      },
+      column8: {
+        startWidth: 400,
+        sortable: true,
+        sortDirection: 'ascending', // 'descending'
+        sortWeight: 'primary', // 'secondary', 'tertiary'
+        resize: true,
+        title: 'Column 8',
+        component: <div>Column 8</div>,
+      },
+    };
+
+    const generateRows = num => (new Array(num)).fill().map((val, index) => (
+      {
+        column1: {
+          title: `Row ${index} Column 0`,
+        },
+        column2: {
+          title: `Row ${index} Column 1`,
+        },
+        column3: {
+          title: `Row ${index} Column 2`,
+        },
+        column4: {
+          title: `Row ${index} Column 3`,
+        },
+        column5: {
+          title: `Row ${index} Column 4`,
+        },
+        column6: {
+          title: `Row ${index} Column 5`,
+        },
+        column7: {
+          title: `Row ${index} Column 6`,
+        },
+        column8: {
+          title: `Row ${index} Column 7`,
+        },
+      }
+    ));
+
+    const rows = generateRows(30);
+
+    return (
+      <div>
+        <div style={{ height: '500px' }}>
+          <DataGrid
+            columns={columns}
+            fixedColumnKeys={['column1', 'column2', 'column8']}
+            flexColumnKeys={['column3', 'column4', 'column5', 'column6', 'column7']}
+            rowSize="33px"
+            rows={rows}
+          />
+        </div>
+        <br />
+        <br />
+        <br />
+        <div id="data-grid-test" />
+      </div>
+    );
+  }
+}
+
+
+export default DataGridStandard;
--- packages/terra-data-grid/src/DataGrid.jsx
+++ packages/terra-data-grid/src/DataGrid.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import ContentContainer from 'terra-content-container';
 
 import styles from './DataGrid.scss';
 
@@ -10,31 +11,53 @@ const propTypes = {
   test: PropTypes.string,
 };
 
-const Column = ({ name, cellCount }) => (
-  <div className={cx('column')}>
-    {(new Array(cellCount)).fill().map((derp, index) => index === 0 ? (
-      <div className={cx('header-cell')}>{name}</div>
-    ) : (
-      <div className={cx('cell')}>Cell {index}</div>
-    ))}
+const CustomCell = ({ text }) => (
+  <div className={cx('custom-cell')}>
+    {text}
   </div>
 );
 
-const columnSet = (num) => {
-  const columns = [];
-  for (let i = 0; i < num; i += 1) {
-    columns.push((
-      <Column name={`Column ${i}`} cellCount={1000} />
-    ));
-  }
-  return columns;
-};
+// const Row = ({ name, columnCount }) => (
+//   <div className={cx(['row'])}>
+//     {(new Array(columnCount)).fill().map((derp, index) => index === 0 ? (
+//       <div className={cx(['cell', 'start-cell'])}><CustomCell text={name} /></div>
+//     ) : (
+//       <div className={cx('cell')}><CustomCell text={`Cell ${index} skdfj;a lskdjf;alkjsdf;lajk sd;fljk as;dljf a;lsdkj f;alksj df;lajk sdf;lajk sdf`} /></div>
+//     ))}
+//   </div>
+// );
+
+// const HeaderRow = ({ name, columnCount }) => (
+//   <div className={cx(['row', 'header-row'])}>
+//     {(new Array(columnCount)).fill().map((derp, index) => index === 0 ? (
+//       <div className={cx(['header-cell', 'start-cell'])}><CustomCell text={name} /></div>
+//     ) : (
+//       <div className={cx('header-cell')}><CustomCell text={`Cell ${index}`} /></div>
+//     ))}
+//   </div>
+// );
+
+// const SectionHeader = () => (
+//   <div className={cx('section-header')}>
+//     <div className={cx('section-header-fixed')}>
+//       Section Header that is long long long long long long long long long long long long long long long long long long long long long
+//     </div>
+//   </div>
+// );
+
+// const rowSet = (num) => {
+//   const rows = [];
+//   for (let i = 0; i < num; i += 1) {
+//     rows.push((
+//       <Row name={`Row ${i}`} columnCount={100} />
+//     ));
+//   }
+//   return rows;
+// };
 
 class DataGrid extends React.Component {
   constructor(props) {
     super(props);
-
-    this.updateRefs = this.updateRefs.bind(this);
 
     this.state = {
 
@@ -42,31 +65,83 @@ class DataGrid extends React.Component {
   }
 
   componentDidMount() {
-    this.updateRefs();
   }
 
-  updateRefs() {
-    if (this.fixedRef) {
-      this.fixedRef.style.height = `${this.fixedRef.scrollHeight}px`;
-
-      if (this.filledRef) {
-        this.filledRef.style.height = `${this.fixedRef.scrollHeight}px`;
-      }
-    }
-  }
-
-  componentDidUpdate() {
-    this.updateRefs();
-  }
 
   render() {
-    return (
-      <div className={cx(['container', 'container-size-1'])}>
-        <div ref={(fixedRef) => { this.fixedRef = fixedRef; }} className={cx('fixed')}>
-          {columnSet(3)}
+    const { columns, rows, fixedColumnKeys, flexColumnKeys, rowSize } = this.props;
+
+    const fixedColumnWidth = fixedColumnKeys.map(key => columns[key].startWidth).reduce((totalWidth, width) => totalWidth + width);
+
+    const headerRow = (
+      <div className={cx(['row', 'header-row'])}>
+        <div className={cx(['fixed-column-container'])} style={{ width: `${fixedColumnWidth}px` }}>
+          {fixedColumnKeys.map((columnKey) => {
+            const columnData = columns[columnKey];
+
+            return (
+              <div className={cx(['header-cell'])} style={{ width: columnData.startWidth }}>
+                <CustomCell text={columnData.title} />
+              </div>
+            );
+          })}
         </div>
-        <div ref={(filledRef) => { this.filledRef = filledRef; }} className={cx('fill')}>
-          {columnSet(30)}
+        {flexColumnKeys.map((columnKey) => {
+          const columnData = columns[columnKey];
+
+          return (
+            <div className={cx(['header-cell'])} style={{ width: columnData.startWidth }}>
+              <CustomCell text={columnData.title} />
+            </div>
+          );
+        })}
+      </div>
+    );
+
+    const contentRows = rows.map((row, index) => {
+      const fixedColumnRowData = [];
+      fixedColumnKeys.forEach(fixedColumnKey => (
+        fixedColumnRowData.push(row[fixedColumnKey])
+      ));
+
+      const flexColumnRowData = [];
+      flexColumnKeys.forEach(flexColumnKey => (
+        flexColumnRowData.push(row[flexColumnKey])
+      ));
+
+      return (
+        <div className={cx(['row', { 'stripe-row': index % 2 > 0 }])}>
+          <div className={cx(['fixed-column-container'])} style={{ width: `${fixedColumnWidth}px` }}>
+            {fixedColumnKeys.map((columnKey) => {
+              const columnData = columns[columnKey];
+              const rowData = row[columnKey];
+
+              return (
+                <div className={cx(['cell'])} style={{ width: columnData.startWidth }}>
+                  <CustomCell text={rowData.title} />
+                </div>
+              );
+            })}
+          </div>
+          {flexColumnKeys.map((columnKey) => {
+            const columnData = columns[columnKey];
+            const rowData = row[columnKey];
+
+            return (
+              <div className={cx(['cell'])} style={{ width: columnData.startWidth }}>
+                <CustomCell text={rowData.title} />
+              </div>
+            );
+          })}
+        </div>
+      );
+    });
+
+    return (
+      <div className={cx(['container'])}>
+        <div ref={(contentWrapper) => { this.contentWrapper = contentWrapper; }} className={cx(['wrapper'])} style={{ marginLeft: `${fixedColumnWidth}px` }}>
+          {headerRow}
+          {contentRows}
         </div>
       </div>
     );
