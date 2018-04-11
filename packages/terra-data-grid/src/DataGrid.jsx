428eb2871c298eee3eb2beb207c77ed6e714cb4a
tbiethman <tbiethman@gmail.com>
2018-04-11 12:15:59 -0500

    Why oh why

--- packages/terra-data-grid/src/DataGrid.jsx
+++ packages/terra-data-grid/src/DataGrid.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import ContentContainer from 'terra-content-container';
 import { DraggableCore } from 'react-draggable';
 
 import styles from './DataGrid.scss';
@@ -22,6 +23,8 @@ class DataGrid extends React.Component {
     super(props);
 
     this.updateWidths = this.updateWidths.bind(this);
+    this.handleHorizontalScroll = this.handleHorizontalScroll.bind(this);
+
 
     const fixedColumnWidth = props.fixedColumnKeys.map(key => props.columns[key].startWidth).reduce((totalWidth, width) => totalWidth + width);
 
@@ -34,6 +37,41 @@ class DataGrid extends React.Component {
       fixedColumnWidth,
       columnWidths,
     };
+  }
+
+  componentDidMount() {
+    this.verticalOverflowContainer.addEventListener('scroll', this.handleHorizontalScroll);
+  }
+
+  componentWillUnmount() {
+    this.verticalOverflowContainer.removeEventListener('scroll', this.handleHorizontalScroll);
+  }
+
+  handleHorizontalScroll() {
+    if (!this.isScrolling) {
+      this.isScrolling = true;
+
+      this.overflowHeaderContainer.style.display = 'none';
+      this.fixedHeaderContainer.style.display = 'block';
+
+      this.fixedOverflowContainer.scrollLeft = this.horizontalOverflowContainer.scrollLeft;
+    }
+
+    if (this.verticalScrollTimeout) {
+      clearTimeout(this.verticalScrollTimeout);
+    }
+
+    this.verticalScrollTimeout = setTimeout(() => {
+      this.fixedHeaderContainer.style.display = 'none';
+
+      this.overflowHeaderContainer.style.display = 'block';
+      this.overflowHeaderContainer.style.position = 'relative';
+      this.overflowHeaderContainer.style.top = `${this.verticalOverflowContainer.scrollTop}px`;
+
+      this.fixedOverflowContainer.scrollLeft = this.horizontalOverflowContainer.scrollLeft;
+
+      this.isScrolling = false;
+    }, 100);
   }
 
   updateWidths(columnKey, widthDelta) {
@@ -164,12 +202,58 @@ class DataGrid extends React.Component {
     });
 
     return (
-      <div className={cx(['container'])}>
-        <div ref={(contentWrapper) => { this.contentWrapper = contentWrapper; }} className={cx(['wrapper'])} style={{ marginLeft: `${fixedColumnWidth}px` }}>
-          {headerRow}
-          {contentRows}
+      <ContentContainer
+        header={(
+          <div className={cx(['container'])}>
+            <div ref={(ref) => { this.fixedOverflowContainer = ref; }} className={cx(['wrapper'])} style={{ marginLeft: `${fixedColumnWidth}px`, overflow: 'hidden' }}>
+              <div ref={(ref) => { this.fixedHeaderContainer = ref; if (ref) { this.fixedHeaderContainer.style.display = 'none'; } }}>
+                {headerRow}
+              </div>
+            </div>
+          </div>
+        )}
+        fill
+      >
+        <div ref={(ref) => { this.verticalOverflowContainer = ref; }} className={cx(['container'])}>
+          <div style={{ position: 'absolute', left: '0', top: '0', width: `${fixedColumnWidth}px`, display: 'flex' }}>
+            {fixedColumnKeys.map((columnKey) => {
+              const columnData = columns[columnKey];
+
+              return (
+                <div className={cx(['header-cell', 'selectable'])} style={{ width: `${this.state.columnWidths[columnKey]}px` }} tabIndex="0">
+                  <CustomCell text={columnData.title} />
+                  <DraggableCore
+                    handle=".drag-handle"
+                    onStart={(event, data) => {
+                      data.node.classList.add(cx('react-draggable-dragging'));
+                      this.scrollPosition = 0;
+                    }}
+                    onStop={(event, data) => {
+                      data.node.classList.remove(cx('react-draggable-dragging'));
+                      data.node.style.transform = '';
+                      this.updateWidths(columnKey, this.scrollPosition);
+                    }}
+                    onDrag={(event, data) => {
+                      this.scrollPosition += data.deltaX;
+                      data.node.style.transform = `translate3d(${this.scrollPosition}px, 0, 100px)`;
+                    }}
+                  >
+                    <div className={cx(['drag-header', 'drag-handle'])}>
+                      <div className={cx('inner-drag')} />
+                    </div>
+                  </DraggableCore>
+                </div>
+              );
+            })}
+          </div>
+          <div ref={(ref) => { this.horizontalOverflowContainer = ref; }} className={cx(['wrapper'])} style={{ marginLeft: `${fixedColumnWidth}px` }}>
+            <div ref={(ref) => { this.overflowHeaderContainer = ref; }}>
+              {headerRow}
+            </div>
+            {contentRows}
+          </div>
         </div>
-      </div>
+      </ContentContainer>
     );
   }
 }
